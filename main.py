bbf2b6ce3036b4fcac1f277879b9d59051d797a1
caio <000109324751SP@al.educacao.sp.gov.br>
2026-08-28 16:41:06 -0300

    semana18

--- main.py
+++ main.py
@@ -1,38 +1,24 @@
+def somar (valor_principal, acrescimo):
+    '''
+    Adiciona o acrescimo ao valor pricipal
 
+    Arg:
+        valor_principal (float):valor inicil da soma
+        acrescimo (float):valor a ser adicinado a valor inicial
 
-def CalcularValorFinal(valor_base = 0.0, taxa_desconto = 0.0):
-    valor_com_desconto = valor_base - (valor_base * (taxa_desconto / 100))
-    return valor_com_desconto
+        Returnns
+            float:somado do valor principal e acrescimo
+    '''
+    return valor_principal + acrescimo
 
-def ExibirRecibo(nome_produto = '', valor_pago = 0.0):
-    print(f'{nome_produto} - {valor_pago:.2f}')
+def testar_operacao_soma():
+    # Arrage
+    valor1=100.0
+    valor2=50.0
+    # Act
+    resultado =somar(valor1 ,valor2)
+    # Assert
+    assert resultado ==150.0, "A soma falhou!"
+    print("Tete de soma: PASSOU!")
 
-while True:
-    nome = input('Digite o nome do produto: ')
-    preco = float(input('Digite o valor do produto: '))
-    desconto = float(input('Digite a taxa de desconto: '))
-
-    preco_final = CalcularValorFinal(preco, desconto)
-
-    produto = [nome, preco, desconto, preco_final]
-    produtos.append(produto)
-
-    opcao = input('Deseja incluir um novo produto? s/n: ')
-
-    if opcao.lower() != 's':
-        for produto in produtos:
-            ExibirRecibo(produto[0], produto[3])
-        break
-    v=100 # define v como 100
-    x=x+1 # soma 1 ao x
-    limite_tentativas_login=10
-    valobase=0.0
-    def CalcularValorfinal(valoro_base,taxa_desconto):
-        '''Aplica o desconto a valor base
-            Args:
-                valor_base(float): valor original do produto
-                taxa_desconto(float):taxa de desconto a ser aplicada
-
-            Returns:
-                float:valor base com desconto
-        '''
+testar_operacao_soma()
